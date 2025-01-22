a858dd19453414231ddd586421642a4dc95c1d05
Ralf <rh@leancoders.de>
2025-01-22 13:40:23 +0100

    wip

--- magento-client-core/src/test/java/de/leancoders/magento/common/MagentoClientCategoryUnitTest.java
+++ magento-client-core/src/test/java/de/leancoders/magento/common/MagentoClientCategoryUnitTest.java
@@ -8,6 +8,7 @@ import de.leancoders.magento.client.helper.jackson.ObjectMapperFactory;
 import de.leancoders.magento.common.model.category.Category;
 import de.leancoders.magento.common.model.category.CategoryProduct;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -22,6 +23,7 @@ public class MagentoClientCategoryUnitTest {
     private static final ObjectMapper OBJECT_MAPPER = ObjectMapperFactory.createDefaultObjectMapper();
     private static final Mediator LOCAL_ADMIN = Mediator.localAdmin();
 
+    @DisplayName("Category: Fetch by ID")
     @Test
     public void test_get_category_by_id() throws JsonProcessingException {
         final long id = 15;
@@ -35,6 +37,7 @@ public class MagentoClientCategoryUnitTest {
         log.info("category:\r\n{}", OBJECT_MAPPER.writeValueAsString(category));
     }
 
+    @DisplayName("Category: Delete by ID")
     @Test
     public void test_delete_category_by_id() throws JsonProcessingException {
         final long id = 15;
@@ -45,6 +48,7 @@ public class MagentoClientCategoryUnitTest {
         log.info("category deleted: {}", (Object) deleted);
     }
 
+    @DisplayName("Category: Listing")
     @Test
     public void test_list_categories() throws JsonProcessingException {
         final MagentoClient client = new MagentoClient(LOCAL_ADMIN.getUrl());
@@ -54,6 +58,7 @@ public class MagentoClientCategoryUnitTest {
         log.info("categories: {}\r\n", OBJECT_MAPPER.writeValueAsString(page));
     }
 
+    @DisplayName("Category: Product Listing in Category by ID")
     @Test
     public void test_list_products_in_category() throws JsonProcessingException {
         long id = 15;
@@ -64,6 +69,7 @@ public class MagentoClientCategoryUnitTest {
         log.info("products in category 15:\r\n{}", OBJECT_MAPPER.writeValueAsString(products));
     }
 
+    @DisplayName("Category: Product Addition in Category by ID")
     @Test
     public void add_product_to_category() throws JsonProcessingException {
         final long categoryId = 15;
@@ -75,6 +81,7 @@ public class MagentoClientCategoryUnitTest {
         log.info("added ? {}", (Object) added);
     }
 
+    @DisplayName("Category: Product Removal in Category by ID")
     @Test
     public void delete_product_from_category() throws JsonProcessingException {
         final long categoryId = 15;
